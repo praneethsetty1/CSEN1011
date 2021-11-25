7fa09b3e446283c37b6e2d9e49e69971626db7df
praneethsetty1 <93368077+praneethsetty1@users.noreply.github.com>
2021-11-25 09:30:29 +0000

    avrmarks.c

---
+++ avgmarks.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main() {
+int a,b,c,d,e;
+printf("marks in a=");
+scanf("%d", &a);
+printf("marks in b=");
+scanf("%d", &b);
+printf("marksin c=");
+scanf("%d" , &c);
+printf("marks in d=");
+scanf("%d", &d);
+printf("marks in e=");
+scanf("%d", &e);
+ printf("the average of all =%d", (a+b+c+d+e)/5);
+}

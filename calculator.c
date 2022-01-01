6eced4f13f6277158424850d026ffd9ed86f084e
praneeth setty <93368077+praneethsetty1@users.noreply.github.com>
2022-01-01 20:35:55 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -5,30 +5,3 @@ Algorithm:
 03. Based on the operator use switch case
 04. Stop  */
 
-# include <stdio.h>
-int main() {
-  int a, b;
-  char op;
-  
-  printf("Enter two numbers (a:b) :" );
-  scanf("%d:%d", &a, &b);
-  
-  printf("Enter an operator +, -, *, /" );
-  scanf("%c", &op);
-    
-  switch (op) {
-    case '+': printf("%d", a + b);
-              break;
-    case '-': printf("%d", a - b);
-              break;
-    case '*': printf("%d", a * b);
-              break;
-    case '/': printf("%d", a / b);
-              break;
-    case '%': printf("%d", a % b);
-              break;
-    default: printf("Inalid operator");
-  }
-
-  return 0;
-}
